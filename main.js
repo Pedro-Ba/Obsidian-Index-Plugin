a4c80ff6329e23b8ce90651206057217711130b4
Pedro B <pedropaschbarraza@gmail.com>
2024-09-12 08:42:19 -0300

    Small changes to file creation

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class myPlugin extends obsidian.Plugin {
         this.registerEvent(
             this.app.workspace.onLayoutReady(() => {
                 this.app.vault.on("create", async () => {
-                    console.log("You created a file");
+                    console.log("You created a file"); //can this also happen on a folder creation? Need to check these.
                     //get where the file was created
                     let currFolder = this.app.workspace.getActiveFile().parent.path; //current folder path
                     let existsIndex = await this.app.vault.adapter.exists(currFolder + "/index.md");
@@ -21,13 +21,17 @@ class myPlugin extends obsidian.Plugin {
                         this.app.vault.append(file, `[[${this.app.workspace.getActiveFile().basename}]]\n`)
                     }
                     else{
-                        //this should never happen because of the command we previously created, so I'm not going to treat this right now.
+                        //in case index.md file does not exist, then we also assume that it's because we've created this folder recently, and thus index.md file does not exist.
+                        //What this means is that we should actually look out and create an index.md file inside a folder when a folder is created, but that's not really relevant to the case right now?
+                        //anyway, if you fell here, index.md does not exist for some reason, so we create index.md and then add the file.
+                        this.app.vault.create(currFolder + "/index.md", `[[${this.app.workspace.getActiveFile().basename}]]`);
                     }
                 })
                 this.app.vault.on("delete", () => {
                     console.log("You deleted a file.");
                     //remove the file from the index.md? Kinda lazy to do this rn.
                 })
+                //need an extra one for when the file is moved.
             })
         );
         this.addCommand({
