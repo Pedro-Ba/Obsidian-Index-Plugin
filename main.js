b42a937783af11376e4baa3ac6cfcd8808f0f696
Estia <83793315+Pedro-Ba@users.noreply.github.com>
2024-09-14 08:24:58 -0300

    Change index naming for ease of use

--- main.js
+++ main.js
@@ -14,17 +14,17 @@ class myPlugin extends obsidian.Plugin {
                     console.log("You created a file"); //can this also happen on a folder creation? Need to check these.
                     //get where the file was created
                     let fileFolder = file.parent.path; //path of created file; maybe change for string treatment (remove after last slash?)
-                    let existsIndex = await this.app.vault.adapter.exists(fileFolder + "/index.md");
+                    let existsIndex = await this.app.vault.adapter.exists(`${fileFolder}/index-${file.parent.name}.md`);
                     if(existsIndex){
                         //just add it to the end of the index file
-                        let indexFile = this.app.vault.getFileByPath(fileFolder + "/index.md");
+                        let indexFile = this.app.vault.getFileByPath(`${fileFolder}/index-${file.parent.name}.md`);
                         this.app.vault.append(indexFile, `[[${file.basename}]]\n`)
                     }
                     else{
                         //in case index.md file does not exist, then we also assume that it's because we've created this folder recently, and thus index.md file does not exist.
                         //What this means is that we should actually look out and create an index.md file inside a folder when a folder is created, but that's not really relevant to the case right now?
                         //anyway, if you fell here, index.md does not exist for some reason, so we create index.md and then add the file.
-                        this.app.vault.create(fileFolder + "/index.md", `[[${this.app.workspace.getActiveFile().basename}]]`);
+                        // this.app.vault.create(fileFolder + "/index.md", `[[${this.app.workspace.getActiveFile().basename}]]`);
                     }
                 })
                 this.app.vault.on("delete", () => {
@@ -39,9 +39,9 @@ class myPlugin extends obsidian.Plugin {
             name: "Creates and updates all indexes in folders.",
             callback: async () => {
                 for(let folder of this.app.vault.getAllFolders()){
-                    let existsIndex = await this.app.vault.adapter.exists(folder.path + "/index.md");
+                    let existsIndex = await this.app.vault.adapter.exists(`${folder.path}/index-${folder.name}.md`);
                     if(existsIndex){
-                        console.log(`The folder ${folder.path} contains an index.md file.`);
+                        console.log(`The folder ${folder.path} contains an index file.`);
                     }
                     else{
                         let file = "";
@@ -50,7 +50,7 @@ class myPlugin extends obsidian.Plugin {
                                 file += `[[${child.basename}]]\n`;
                             }
                         }
-                        this.app.vault.create(folder.path + "/index.md", file);
+                        this.app.vault.create(`${folder.path}/index-${folder.name}.md`, file);
                     }
                 }
             }
